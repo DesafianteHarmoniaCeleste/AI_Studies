04e7949bbf013b69ce2b45397d8fbbd1004b4ffa
Pedro Gasparelo Leme <127804307+DesafianteHarmoniaCeleste@users.noreply.github.com>
2026-04-11 11:57:18 -0300

    Create L1_E4.pl

---
+++ L1_E1/L1_E4.pl
@@ -0,0 +1,69 @@
+Uma Base de Dados (BD) pode ser representada em Prolog como um conjunto de fatos. A partir desses
+fatos pode ser obtida informações e por meio de relações conhecimento ser representado e manipulado.
+Por exemplo, uma BD sobre alunos pode ser representada tal que cada aluno é descrito por uma cláusula.
+Considerar que cada aluno tem os seguintes componentes: nome, endereço, telefone, data de nascimento,
+t1, p1, t2, p2, sub, rec, freq. Por exemplo:
+
+aluno(“Carlos Silva”, “Rua das Orquideas, 32”, “016966666666”, “05/05/1955”,
+t1,p1,t2,p2,sub,rec,freq).
+
+I. Desenvolva programas que informem:
+
+A. a média de prova de um aluno, tendo a p1 peso 2 e a p2 peso 3 e considerando
+a possibilidade da sub.
+
+media_prova(Nome, Media) :-
+    aluno(Nome, _, _, _, _, P1, _, P2, Sub, _, _),
+
+    % trata substitutiva
+    (
+        Sub >= P1, P1 =< P2 -> P1n = Sub, P2n = P2 ;
+        Sub >= P2, P2 < P1  -> P2n = Sub, P1n = P1 ;
+        P1n = P1, P2n = P2
+    ),
+
+    Media is (P1n*2 + P2n*3) / 5.
+
+B. a média de trabalho de um aluno, tendo o t1 peso 1 e o t2 peso 3.
+
+media_trabalho(Nome, Media) :-
+    aluno(Nome, _, _, _, T1, _, T2, _, _, _, _),
+    Media is (T1 + 3*T2) / 4.
+
+C. a média final de um aluno, considerando a situação antes da rec.
+
+media_final(Nome, MediaFinal) :-
+    media_prova(Nome, MP),
+    media_trabalho(Nome, MT),
+    MediaFinal is (MP + MT) / 2.
+
+D. se o aluno foi aprovado ou reprovado, considerando a situação antes da rec.
+
+situacao(Nome, aprovado) :-
+    media_final(Nome, MF),
+    aluno(Nome, _, _, _, _, _, _, _, _, _, Freq),
+    MF >= 5,
+    Freq >= 75.
+
+situacao(Nome, reprovado) :-
+    media_final(Nome, MF),
+    aluno(Nome, _, _, _, _, _, _, _, _, _, Freq),
+    (MF < 5 ; Freq < 75).
+
+II. Mostre as interrogações (e os resultados) para determinar:
+
+A. Quem tirou 10 na p1 e na p2?
+B. Quem tirou 10 no t1 e no t2?
+C. Quem ficou com nota abaixo da média (abaixo de 5), nas duas provas? (p1<5 e
+p2<5).
+D. Quem ficou com presença abaixo de 70%?
+E. Quais foram os alunos reprovados?
+F. Quais foram os alunos aprovados?
+
+III. Indique as alterações necessárias na estrutura aluno para que seja possível fazer as
+seguintes interrogações:
+A. Quais são as informações dos alunos cujo primeiro nome é X?
+B. Quais são as informações dos alunos cujo sobrenome é X?
+C. Quem são os alunos nascidos no ano X?
+D. Quem são os alunos cujo CEP é X?
+E. Qual o número da casa do aluno X?
